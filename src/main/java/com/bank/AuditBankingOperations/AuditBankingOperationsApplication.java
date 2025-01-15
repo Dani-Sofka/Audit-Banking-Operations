9f4456b2074d91cebede68692b83cc68c9287baf
daniel.rua@sofka.com.co <daniel.rua@sofka.local>
2025-01-14 23:48:57 -0500

    first commit

---
+++ src/main/java/com/bank/AuditBankingOperations/AuditBankingOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.bank.AuditBankingOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditBankingOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditBankingOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/AuditBankingOperations/AuditBankingOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.AuditBankingOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditBankingOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
